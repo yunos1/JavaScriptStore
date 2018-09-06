68f9278955610c7d053eaca3d0abc0f4eac8a7e9
LiuQZ <1562616404@qq.com>
2018-09-06 21:31:53 +0800

    练习

---
+++ 博雅互动(优化中)/js/relation.js
@@ -0,0 +1,11 @@
+window.onload =function(){
+	function $(id){
+		return document.getElementById(id);
+	}
+	
+	$('content').innerHTML = '<h1>Welcome to the rookie alliance</h1><br/>';
+	$('content1').innerHTML = '<h1> Please enjoy the Smurfs</h1><br/>';
+	$('content2').innerHTML = '<h1>Liu QZ</h1>';
+							 
+	
+}
